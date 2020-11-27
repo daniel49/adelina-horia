d2a5d7c099850db10a751a31f1d84f5c2dbe5b6e
Daniel <ilie.daniel49@yahoo.com>
2020-11-27 19:50:32 +0200

    finished fourth layer

--- src/layers/fourthLayer/fourthLayer.js
+++ src/layers/fourthLayer/fourthLayer.js
@@ -5,6 +5,8 @@ import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople";
 import AccountBalanceIcon from "@material-ui/icons/AccountBalance";
 import MusicNoteIcon from "@material-ui/icons/MusicNote";
 import ChurchImage from "./church.jpeg";
+import BallroomImage from "./ballroom.jpeg";
+import Grid from "@material-ui/core/Grid";
 
 
 class FourthLayer extends React.Component {
@@ -27,82 +29,109 @@ class FourthLayer extends React.Component {
                 </div>
 
                 <div className={"fourth-layer-1"}>
-                    <ScrollAnimation
-                        className={"fourth-layer-center-animation"}
-                        animateIn='wobble'
-                        initiallyVisible={true}
-                        afterAnimatedIn={() => !this.state.showFirstPart ?
-                            this.setState({showFirstPart: true}) : null}
-                    >
-                        <EmojiPeopleIcon className={"fourth-layer-symbol"}/>
-                    </ScrollAnimation>
-
-                    {
-                        this.state.showFirstPart && (
+                    <Grid container alignItems={"center"}>
+                        <Grid item xs={0} md={0} lg={4} alignItems={"center"} />
+                        <Grid item xs={12} md={4} lg={4} alignItems={"center"}>
                             <ScrollAnimation
-                                animateIn='fadeIn'
-                                animateOut='fadeOut'
-                                animateOnce={true}
+                                className={"fourth-layer-center-animation"}
+                                animateIn='wobble'
+                                initiallyVisible={true}
+                                afterAnimatedIn={() => !this.state.showFirstPart ?
+                                    this.setState({showFirstPart: true}) : null}
                             >
-                                <div className={"fourth-layer-first-part"}>
-                                    <p className={"fourth-layer-first-part-text"}>Asteptam cu nerabdare ziua de <br/> <b>19 IULIE 2021</b><br/> sa ne vedem cu bine <br/> si plini de energie</p>
-                                </div>
+                                <EmojiPeopleIcon className={"fourth-layer-symbol"}/>
                             </ScrollAnimation>
-                        )
-                    }
+                        </Grid>
+                        <Grid item xs={12} md={8} lg={4} alignItems={"center"}>
+                            {
+                                this.state.showFirstPart && (
+                                    <ScrollAnimation
+                                        animateIn='fadeIn'
+                                        animateOut='fadeOut'
+                                        animateOnce={true}
+                                    >
+                                        <div className={"fourth-layer-first-part"}>
+                                            <p className={"fourth-layer-first-part-text"}><b>Instructiuni</b><br/>Asteptam cu nerabdare ziua de <br/>
+                                            <b>19 IULIE 2021</b><br/> sa ne vedem cu bine si plini de energie<br/>caci dansul este obligatoriu</p>
+                                        </div>
+                                    </ScrollAnimation>
+                                )
+                            }
+                        </Grid>
+                    </Grid>
                 </div>
 
                 <div className={"fourth-layer-2"}>
-                    <ScrollAnimation
-                        className={"fourth-layer-center-animation"}
-                        animateIn='bounce'
-                        initiallyVisible={true}
-                        afterAnimatedIn={() => !this.state.showSecondPart ?
-                            this.setState({showSecondPart: true}) : null}
-                    >
-                        <AccountBalanceIcon className={"fourth-layer-symbol"}/>
-                    </ScrollAnimation>
+                    <Grid container alignItems={"center"}>
+                        <Grid item xs={12} md={8} lg={4} alignItems={"center"}>
+                            {
+                                this.state.showSecondPart && (
+                                    <ScrollAnimation
+                                        animateIn='flipInY'
+                                        animateOut='fadeOut'
+                                        animateOnce={true}
+                                    >
+                                        <div className={"fourth-layer-second-part"}>
+                                            <img className={"fourth-layer-church-image"} src={ChurchImage} alt={"church"} />
+                                        </div>
+                                        <p className={"fourth-layer-second-part-text"}>
+                                            <b>Cununia religioasa</b><br/>Mergem si ne atestam dragostea<br/>in fata lui Dumnezeu la<br/>
+                                            <b>Catedrala "Nasterea Maicii Domnului"</b><br/>din Gura Humorului
+                                        </p>
+                                    </ScrollAnimation>
 
-                    {
-                        this.state.showSecondPart && (
+                                )
+                            }
+                        </Grid>
+                        <Grid item xs={12} md={4} lg={4} alignItems={"center"}>
                             <ScrollAnimation
-                                animateIn='fadeIn'
-                                animateOut='fadeOut'
-                                animateOnce={true}
+                                className={"fourth-layer-center-animation"}
+                                animateIn='bounce'
+                                initiallyVisible={true}
+                                afterAnimatedIn={() => !this.state.showSecondPart ?
+                                    this.setState({showSecondPart: true}) : null}
                             >
-                                <div className={"fourth-layer-second-part"}>
-                                    <img className={"fourth-layer-church"} src={ChurchImage} alt={"church"} />
-                                </div>
+                                <AccountBalanceIcon className={"fourth-layer-symbol"}/>
                             </ScrollAnimation>
-                        )
-                    }
+                        </Grid>
+                        <Grid item xs={0} md={0} lg={4} alignItems={"center"} />
+                    </Grid>
                 </div>
 
                 <div className={"fourth-layer-3"}>
-                    <ScrollAnimation
-                        className={"fourth-layer-center-animation"}
-                        animateIn='tada'
-                        initiallyVisible={true}
-                        afterAnimatedIn={() => !this.state.showThirdPart ?
-                            this.setState({showThirdPart: true}) : null}
-                    >
-                        <MusicNoteIcon className={"fourth-layer-symbol"}/>
-                    </ScrollAnimation>
-
-                    {
-                        this.state.showThirdPart && (
+                    <Grid container alignItems={"center"}>
+                        <Grid item xs={0} md={0} lg={4} alignItems={"center"} />
+                        <Grid item xs={12} md={4} lg={4} alignItems={"center"}>
                             <ScrollAnimation
-                                animateIn='fadeIn'
-                                animateOut='fadeOut'
-                                animateOnce={true}
+                                className={"fourth-layer-center-animation"}
+                                animateIn='tada'
+                                initiallyVisible={true}
+                                afterAnimatedIn={() => !this.state.showThirdPart ?
+                                    this.setState({showThirdPart: true}) : null}
                             >
-                                <div className={"fourth-layer-third-part"}>
-                                </div>
+                                <MusicNoteIcon className={"fourth-layer-symbol"}/>
                             </ScrollAnimation>
-                        )
-                    }
+                        </Grid>
+                        <Grid item xs={12} md={8} lg={4} alignItems={"center"}>
+                            {
+                                this.state.showThirdPart && (
+                                    <ScrollAnimation
+                                        animateIn='flipInX'
+                                        animateOut='fadeOut'
+                                        animateOnce={true}
+                                    >
+                                        <div className={"fourth-layer-second-part"}>
+                                            <img className={"fourth-layer-ballroom-image"} src={BallroomImage} alt={"ballroom"} />
+                                        </div>
+                                        <p className={"fourth-layer-third-part-text"}>
+                                            <b>Receptia & Petrecerea</b><br/>Mergem si petrecem impreuna la<br/>Club Bucovina Ballroom din Gura Humorului<br/>ora <b>XX:YY</b>
+                                        </p>
+                                    </ScrollAnimation>
+                                )
+                            }
+                        </Grid>
+                    </Grid>
                 </div>
-
             </div>
         );
     }
